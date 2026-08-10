5d9807156212888f62ad07969ec66047b2a4e03a
Brennan Jones <brennandgj@gmail.com>
2026-08-10 16:53:14 +0800

    Update Config.ts

--- rn-app/constants/Config.ts
+++ rn-app/constants/Config.ts
@@ -1,1 +1,1 @@
-export const API_URL = 'https://dongkaifyp.brennanjones.com';
+export const API_URL = 'https://vercel.com/brennan-jones-projects-8b223afe/context-aware-ai-journaling-system/825AC4WLZmRNiFQc8LfsNW7CvyCe';
